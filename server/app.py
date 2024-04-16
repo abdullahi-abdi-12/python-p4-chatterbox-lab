b795af62c61c0b476bf6f6487e4b15d735b14cee
abdullahiabdi <cabudicali218@gmail.com>
2024-04-16 12:41:40 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -14,13 +14,38 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods = ['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at.asc()).all()
+        msg_dicts= []
+        for message in messages:
+            msg_dicts.append(message.to_dict())
+        return msg_dicts
+    elif request.method == 'POST':
+        json_data= request.get_json()
+        new_message = Message(
+            body = json_data.get('body'),
+            username = json_data.get('username')
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        return new_message.to_dict(), 201
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods= ['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
 
-if __name__ == '__main__':
-    app.run(port=5555)
+    if message is None:
+        return {'error': 'message not found'}, 404
+    if request.method == 'PATCH':
+        json_data = request.get_json()
+        if 'body' in json_data:
+            message.body = json_data.get('body')
+        db.session.add(message)
+        db.session.commit
+        return message.to_dict(), 200
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        return {'delete': 'message been deleted'}
